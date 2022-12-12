1772233bb545ea7f7e4550136589e4fbf01451cd
nVietUK <vietuk.nguyen.git@icloud.com>
2022-12-12 22:08:53 +0700

    changed

---
+++ install-llvm.bat
@@ -0,0 +1,20 @@
+@echo on
+@call vcvarsall.bat x64 %*
+@call mkdir build\llvm
+@call cd build\llvm
+cmake ^
+    -G "Visual Studio 17 2022" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=C:\\llvm ^
+    -DCMAKE_CXX_STANDARD=17 ^
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
+    -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,$LD_LIBRARY_PATH" ^
+    -DFLANG_ENABLE_WERROR=ON ^
+    -DLLVM_ENABLE_ASSERTIONS=ON ^
+    -DLLVM_TARGETS_TO_BUILD=host ^
+    -DLLVM_LIT_ARGS=-v ^
+    -DLLVM_ENABLE_PROJECTS="clang;mlir;flang;openmp" ^
+    -DLLVM_ENABLE_RUNTIMES="compiler-rt" ^
+    ..\..\llvm\llvm
+@call cd ..\..
+pause
--- installllvm.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-@call vcvarsall.bat x64 %*
-@call cd llvm-project
-cmake -S llvm -B build -G "Visual Studio 17 2022" -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;flang;openmp' -DLLVM_ENABLE_RUNTIMES='openmp' -DCMAKE_INSTALL_PREFIX=C:\\llvm -DCMAKE_BUILD_TYPE=Release 
-pause
