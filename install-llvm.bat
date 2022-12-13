a4202a5803ac0324a25379b1fc9260750c56628e
nVietUK <vietuk.nguyen.git@icloud.com>
2022-12-13 14:39:03 +0700

    update llvm

--- install-llvm.bat
+++ install-llvm.bat
@@ -1,19 +1,12 @@
-@echo off
-@call rmdir build\llvm /s
-@call mkdir build\llvm
-@call cd build\llvm
-cmake ^
-    -G "Visual Studio 17 2022" ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_PREFIX=C:\\llvm ^
-    -DCMAKE_CXX_STANDARD=17 ^
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
-    -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,$LD_LIBRARY_PATH" ^
-    -DFLANG_ENABLE_WERROR=OFF ^
-    -DLLVM_ENABLE_ASSERTIONS=ON ^
-    -DLLVM_TARGETS_TO_BUILD=host ^
-    -DLLVM_LIT_ARGS=-v ^
-    -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld;lldb;mlir;flang;openmp" ^
-    -DLLVM_ENABLE_RUNTIMES="compiler-rt" ^
-    ..\..\llvm\llvm
+@call vcvarsall.bat x64 %*
+@echo on
+@call cd llvm\buildbot
+powershell -Command "(gc configure.py) -replace 'clang;', 'clang;openmp;flang' | Out-File -encoding ASCII configure.py"
+powershell -Command "(gc configure.py) -replace 'abs_obj_dir, \"install\"', '\"c:\", os.sep, \"llvm\"' | Out-File -encoding ASCII configure.py"
+@call cd ..
+rem pause
+python .\buildbot\configure.py --cmake-gen "Visual Studio 17 2022" --cuda -t Release
+@call cd buildbot
+powershell -Command "(gc configure.py) -replace 'clang;openmp;flang', 'clang;' | Out-File -encoding ASCII configure.py"
+powershell -Command "(gc configure.py) -replace '\"c:\", os.sep, \"llvm\"', 'abs_obj_dir, \"install\"' | Out-File -encoding ASCII configure.py"
 @call cd ..\..
