c5c665dc7197452db7ddf2bbc301f199cb965c23
nVietUK <vietuk.nguyen.git@icloud.com>
2022-12-16 15:54:00 +0700

    update but not suẻ that works probably

--- install-magma.bat
+++ install-magma.bat
@@ -2,8 +2,11 @@
 @call rmdir build\magma /s
 @call mkdir build\magma
 
+@call git submodule deinit -f magma
+@call git submodule update --init magma
+
 @call cd magma 
-@call wsl -e echo -e 'BACKEND = cuda\nFORT = true' > make.inc
+@call wsl -e echo -e BACKEND = cuda\nFORT = False > make.inc
 @call wsl -e make generate GPU_TARGET=Turing
 @call cd ..
 
@@ -11,12 +14,14 @@
 @call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2022
 cmake ^
     -G "Visual Studio 17 2022" ^
-    -D CMAKE_CONFIGURATION_TYPES:STRING=Release ^
+    -D CMAKE_CONFIGURATION_TYPES:STRING="Release" ^
     -S ..\..\magma^
-    -DCMAKE_INSTALL_PREFIX=C:\\magma ^
-    -DMAGMA_ENABLE_CUDA=True ^
-    -DCMAKE_CUDA_ARCHITECTURES=75 ^
-    -DGPU_TARGET=Turing ^
-    -D MKLROOT:STRING="C:\Program Files (x86)\Intel\oneAPI\mkl"
+    -D CMAKE_INSTALL_PREFIX:STRING="C:\\magma" ^
+    -D MAGMA_ENABLE_CUDA:BOOL=OFF ^
+    -D CMAKE_CUDA_ARCHITECTURES:STRING="75" ^
+    -D GPU_TARGET:STRING="Turing" ^
+    -D OpenMP_C_FLAGS:STRING="-openmp:llvm" ^
+    -D OpenMP_CXX_FLAGS:STRING="-openmp:llvm" ^
+    -D USE_FORTRAN:BOOL=OFF
 cmake -S ..\..\magma
 @call cd ..\..
