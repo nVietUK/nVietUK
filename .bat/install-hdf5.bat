56168b677d6695968ae99fc84b9cb574b1d4150a
nVietUK <vietuk.nguyen.git@icloud.com>
2022-12-25 16:20:50 +0700

    updat

--- .bat/install-hdf5.bat
+++ .bat/install-hdf5.bat
@@ -5,7 +5,6 @@
 cmake ^
     -G "Visual Studio 17 2022" ^
     -D CMAKE_CONFIGURATION_TYPES:STRING=Release ^
-    -D CMAKE_TRY_COMPILE_CONFIGURATION:STRING=Release ^
     -S ..\..\hdf5^
     -DCMAKE_INSTALL_PREFIX=C:\\hdf5 
 @call cd ..\..
